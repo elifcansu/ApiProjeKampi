58947585224d296d47097f3141fd15869fb8bb5b
Elif Ertürk <elif.erturk@ideabilisim.com>
2025-04-05 01:11:27 +0300

    AutoMapper kütüphanesi eklendi ve GeneralMapping sınıfı oluşturuldu.

--- ApiProjeKampi.WebApi/Program.cs
+++ ApiProjeKampi.WebApi/Program.cs
@@ -1,10 +1,12 @@
 using ApiProjeKampi.WebApi.Context;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddDbContext<ApiContext>(); //ApiContext sýnýfýný Constructor Method olarak kullandýðýmýzý program.cs e söylüyoruz.
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly()); // Þu anda çalýþan assembly’i (yani projenin derlendiði .dll dosyasýný) temsil eder. Bu, AutoMapper'ýn Profile sýnýflarýný bulup otomatik olarak kaydedebilmesi için kullanýlýr.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
